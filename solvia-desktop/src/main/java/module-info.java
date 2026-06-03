c20f95180954d1b31eca7f7af5939e2a34b3fb0a
Alexis <alexis.guinot@onsiea.com>
2026-06-03 23:13:55 +0200

    feature (desktop-theme) : require AtlantaFX module
    Declare the AtlantaFX module dependency required by the desktop dark theme integration.
    
    Refs: desktop-accessibility

--- solvia-desktop/src/main/java/module-info.java
+++ solvia-desktop/src/main/java/module-info.java
@@ -3,8 +3,10 @@ module fr.seynax.solvia.desktop {
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
+    requires atlantafx.base;
     requires javafx.base;
     requires javafx.controls;
+    requires javafx.graphics;
 
     exports fr.seynax.solvia.desktop;
     exports fr.seynax.solvia.desktop.api;
