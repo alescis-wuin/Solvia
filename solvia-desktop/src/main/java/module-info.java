0ba007384af85431296fd489b6e799af91a39130
Alexis <alexis.guinot@onsiea.com>
2026-06-03 22:29:47 +0200

    feature (desktop-mvp) : open desktop module dependencies
    Declare JavaFX, Java HTTP client and Jackson module requirements for the desktop MVP.
    
    Refs: phase-5

--- solvia-desktop/src/main/java/module-info.java
+++ solvia-desktop/src/main/java/module-info.java
@@ -1,5 +1,11 @@
 module fr.seynax.solvia.desktop {
+    requires java.net.http;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
     requires javafx.controls;
 
     exports fr.seynax.solvia.desktop;
+    exports fr.seynax.solvia.desktop.api;
+    exports fr.seynax.solvia.desktop.ui;
 }
